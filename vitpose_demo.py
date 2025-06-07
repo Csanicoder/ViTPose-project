0f97c4116bdbcbc2c5db6dc3d579415e4e0f2d77
repcevol <reccsan@gmail.com>
2025-06-07 20:33:48 +0200

    Clean up tracked files now in .gitignore

--- vitpose_demo.py
+++ vitpose_demo.py
@@ -1,25 +1,35 @@
 import torch
-import requests
 import numpy as np
 
 from PIL import Image
+from sympy.abc import alpha
 
 from transformers import AutoProcessor, RTDetrForObjectDetection, VitPoseForPoseEstimation
 
 import supervision as sv
 import cv2
 
+from os.path import isfile
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-image_path = "images/IMG_20250607_144554.jpg"
-image = Image.open(image_path).convert("RGB")
+#handling input
+image_path = input("Image path here: ")
+
+if image_path == "":
+    image_path = "gitar.jpg"
+
+else:
+    while isfile("/images/"+ image_path):
+        image_path = input("Incorrect image path!\nImage path here: ")
+
+image = Image.open("images/" + image_path).convert("RGB")
 
 # ------------------------------------------------------------------------
 # Stage 1. Detect humans on the image
 # ------------------------------------------------------------------------
 
-# You can choose any detector of your choice
-person_image_processor = AutoProcessor.from_pretrained("PekingU/rtdetr_r50vd_coco_o365")
+person_image_processor = AutoProcessor.from_pretrained("PekingU/rtdetr_r50vd_coco_o365", use_fast = True)
 person_model = RTDetrForObjectDetection.from_pretrained("PekingU/rtdetr_r50vd_coco_o365", device_map=device)
 
 inputs = person_image_processor(images=image, return_tensors="pt").to(device)
@@ -30,7 +40,7 @@ with torch.no_grad():
 results = person_image_processor.post_process_object_detection(
     outputs, target_sizes=torch.tensor([(image.height, image.width)]), threshold=0.3
 )
-result = results[0]  # take first image results
+result = results[0]  # take first image result
 
 # Human label refers 0 index in COCO dataset
 person_boxes = result["boxes"][result["labels"] == 0]
@@ -44,7 +54,7 @@ person_boxes[:, 3] = person_boxes[:, 3] - person_boxes[:, 1]
 # Stage 2. Detect keypoints for each person found
 # ------------------------------------------------------------------------
 
-image_processor = AutoProcessor.from_pretrained("usyd-community/vitpose-base-simple")
+image_processor = AutoProcessor.from_pretrained("usyd-community/vitpose-base-simple", use_fast = True)
 model = VitPoseForPoseEstimation.from_pretrained("usyd-community/vitpose-base-simple", device_map=device)
 
 inputs = image_processor(image, boxes=[person_boxes], return_tensors="pt").to(device)
